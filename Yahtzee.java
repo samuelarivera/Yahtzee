00e16ad79bb0b8cbb8221f1439afa42b0992b1bf
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-12-11 11:44:35 -0800

    carti -m

--- Yahtzee.java
+++ Yahtzee.java
@@ -8,55 +8,60 @@ public class Yahtzee
     private Diex die4;
     private Diex die5;
     private Diex die6;
-    private Diex[] dieset = {die1, die2, die3, die4, die5, die6};
-    public void fullRoll(){
-            die1.roll();
-            die2.roll();
-            die3.roll();
-            die4.roll();
-            die5.roll();
-            die6.roll();
+    private Diex[] dieset;
+
+    public void roll(){
+        die1.roll();
+        die2.roll();
+        die3.roll();
+        die4.roll();
+        die5.roll();
+        die6.roll();
     }
-   
-     public Yahtzee(){
-         die1 = new Diex(6);
-         die2 = new Diex(6);
-         die3 = new Diex(6);
-         die4 = new Diex(6);
-         die5 = new Diex(6);
-         die6 = new Diex(6);
-         this.fullRoll();
+
+    public Yahtzee(){
+        die1 = new Diex(6);
+        die2 = new Diex(6);
+        die3 = new Diex(6);
+        die4 = new Diex(6);
+        die5 = new Diex(6);
+        die6 = new Diex(6);
+        dieset = new Diex[] {die1, die2, die3, die4, die5, die6};
+        this.roll();
     }
-    public void oneRoll(int diceNumber){
+
+    public void roll(int diceNumber){
         if(diceNumber <= 6){
-             dieset[diceNumber -1].roll();
+            dieset[diceNumber -1].roll();
         }
     }
+
     public String summarize(){
         int[] sideArray = {1, 2, 3, 4, 5, 6};
         int[] countArray = {0, 0, 0, 0, 0, 0};
         String organizer = "";
-        for(int dicei = 0; dicei < 5; dicei++){ 
+        for(int x = 0; x < 5; x++){ 
             for(int y = 0; y < 5; y++){
-              if(dieset[dicei].getValue() == sideArray[y]){
-                  countArray[y] = countArray[y] + 1;
-              }
+                if(dieset[x].getValue() == sideArray[y]){
+                    countArray[y] = countArray[y] + 1;
+                }
             }
         }
         for(int i = 1; i < 6; i++){
-            organizer = organizer + i.toString() + "-" + countArray[y - 1].toString + ";" ;
+            organizer = organizer + i + "-" + countArray[i - 1]+ "; " ;
         }
         return organizer;
     }
+
     public String toString(){
         String str = "Dice values;";
         for(int i = 0; i < 5; i++){
-            str = str + " " + dieset[i].toString(); 
+            str = str + " " + dieset[i].getValue(); 
         }
         return str;
     }
-    
-    public int getValue(){
+
+    public int getAllValues(){
         return die1.getValue() + die2.getValue() + die3.getValue() + die4.getValue() + die5.getValue() + die6.getValue();
     }
 
